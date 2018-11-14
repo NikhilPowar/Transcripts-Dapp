2e71b904d4eb43579a678b3da0aa4f46888e7888
nikhilpowar <nikhilpowar16@gmail.com>
2018-11-14 18:52:45 +0530

    Added ID Contracts

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -1,7 +1,12 @@
 var TranscriptApplication=artifacts.require ('./TranscriptApplication.sol');
+var KeyHolder = artifacts.require ('./KeyHolder.sol');
+var KeyHolderLibrary = artifacts.require ('./KeyHolderLibrary');
 
 module.exports = function(deployer, network, accounts) {
     const providerAddress = accounts[1]
     const verifierAddress = accounts[2];
     deployer.deploy(TranscriptApplication, providerAddress, verifierAddress);
+    deployer.deploy(KeyHolderLibrary);
+    deployer.link(KeyHolderLibrary, KeyHolder);
+    deployer.deploy(KeyHolder);
 }
