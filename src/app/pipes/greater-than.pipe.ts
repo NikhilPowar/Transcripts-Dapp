a833bc764c82083b5e1d8398b8a54d64ad96d5bc
Akhil Powar <akhilpowar16@gmail.com>
2018-11-30 22:49:22 +0530

    Added greater than pipe filter

---
+++ src/app/pipes/greater-than.pipe.ts
@@ -0,0 +1,8 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'greaterThan'})
+export class GreaterThanPipe implements PipeTransform {
+    transform(arr: number[], start: number) {
+        return arr.filter(n => n >= start);
+    }
+}
