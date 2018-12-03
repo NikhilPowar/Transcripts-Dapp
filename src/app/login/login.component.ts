7eb5d96f71ef293d06614b4594d60962e6d7e9b0
nikhilpowar <nikhilpowar16@gmail.com>
2018-12-03 18:23:45 +0530

    Fixed routing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -53,8 +53,7 @@ export class LoginComponent implements OnInit {
     this.loginService.login('transcripts', this.usernameString).then((success) => {
       if (success) {
         this.loading = true;
-        // this.router.navigate(['application-list']);
-        this.router.navigate(['admin-page']);
+        this.router.navigate(['application-list']);
       } else {
         this.showPopup = true;
       }
