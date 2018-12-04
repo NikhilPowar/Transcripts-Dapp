7d51ff54410693ef0843efebd693f9144140e804
nikhilpowar <nikhilpowar16@gmail.com>
2018-12-04 21:26:35 +0530

    Subdomain creation now fully functional

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -54,7 +54,7 @@ export class LoginComponent implements OnInit {
     this.loginService.login('transcripts', this.usernameString).then((success) => {
       if (success) {
         this.loading = true;
-        this.router.navigate(['application-list']);
+        this.router.navigate(['user-name']);
       } else {
         this.showPopup = true;
       }
